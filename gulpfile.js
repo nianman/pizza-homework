5c4586c0117e5ebd8f09c119905d6b09df13a9b5
yamnyk.vlog <yamnyk.vlog@gmail.com>
2019-11-19 13:36:18 +0400

    add browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,20 @@
 const gulp = require('gulp'),
       sass = require('gulp-sass'),
-      prefixer = require('gulp-autoprefixer');
+      prefixer = require('gulp-autoprefixer'),
+      browserSync = require('browser-sync').create();
 
 const path = {
     dist: {
+        self: "./dist/",
         html: "dist/",
         css: "dist/css/"
     },
     src: {
         html: "src/**/*.html",
         scss: "src/scss/style.scss"
+    },
+    watch: {
+        scss: "src/**/*.scss"
     }
 };
 
@@ -18,6 +23,7 @@ const buildHTML = () => (
     gulp.src(path.src.html)
         .pipe(gulp.dest(path.dist.html))
 );
+
 const buildSCSS = () => (
     gulp.src(path.src.scss)
         .pipe(sass().on('error', sass.logError))
@@ -27,11 +33,23 @@ const buildSCSS = () => (
         .pipe(gulp.dest(path.dist.css))
 );
 
+const watcher = () => {
+    browserSync.init({
+        server: {
+            baseDir: path.dist.self
+        }
+    });
+
+    gulp.watch(path.src.html, buildHTML).on('change', browserSync.reload);
+    gulp.watch(path.watch.scss, buildSCSS).on('change', browserSync.reload);
+};
+
 /*** CREATING TASKS ***/
 gulp.task('html', buildHTML);
 gulp.task('scss', buildSCSS);
 
 gulp.task('default', gulp.series(
     buildHTML,
-    buildSCSS
+    buildSCSS,
+    watcher
 ));
